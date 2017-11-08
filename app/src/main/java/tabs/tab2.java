8b6994dec30ed969cbdf64cf4a421abcb22f447f
AleidJana <43200284@student.ksu.edu.sa>
2017-11-08 13:12:04 +0300

    the tabs fragments code

---
+++ app/src/main/java/tabs/tab2.java
@@ -0,0 +1,21 @@
+package tabs;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.lama.realestateapp.R;
+
+
+public class tab2 extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle saveInstanceState)
+    {
+        View view = inflater.inflate(R.layout.fragment_tab2,container,false);
+
+        return view;
+    }
+}
