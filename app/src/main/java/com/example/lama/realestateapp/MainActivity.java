6de31a563325efe7049f7fb2f81cbcbc011b709a
AleidJana <43200284@student.ksu.edu.sa>
2017-11-08 13:19:44 +0300

    the tabs main activity java  code

--- app/src/main/java/com/example/lama/realestateapp/MainActivity.java
+++ app/src/main/java/com/example/lama/realestateapp/MainActivity.java
@@ -1,13 +1,35 @@
 package com.example.lama.realestateapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Gravity;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
+Button b;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        android.support.v7.app.ActionBar menu=getSupportActionBar();
+        menu.setDisplayShowHomeEnabled(true);
+        menu.setLogo(R.drawable.ic_action_name);
+        menu.setDisplayUseLogoEnabled(true);
+        b= (Button)findViewById(R.id.btn1);
+        b.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(MainActivity.this, SecoundActivity.class);
+               // i.putExtra("key", array1);
+                startActivity(i);
+            }
+        });
+
+
     }
 }
