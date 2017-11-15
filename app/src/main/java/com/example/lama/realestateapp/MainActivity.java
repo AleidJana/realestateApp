1fdcb1366854d78b64b42f82987888815e0c0810
AleidJana <43200284@student.ksu.edu.sa>
2017-11-15 13:52:20 +0300

    the tabs main activity xml  code

--- app/src/main/java/com/example/lama/realestateapp/MainActivity.java
+++ app/src/main/java/com/example/lama/realestateapp/MainActivity.java
@@ -1,84 +1,108 @@
 package com.example.lama.realestateapp;
 
+import android.app.ProgressDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.AsyncTask;
-import android.provider.Settings;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
-import android.view.Gravity;
+import android.text.Spannable;
+import android.text.SpannableStringBuilder;
+import android.text.style.ForegroundColorSpan;
+import android.util.Log;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.AdapterView;
 import android.widget.AdapterView.OnItemSelectedListener;
-
-
-import org.apache.http.NameValuePair;
-import org.apache.http.client.utils.URLEncodedUtils;
-import org.apache.http.message.BasicNameValuePair;
-import org.json.JSONArray;
-
+import org.xmlpull.v1.XmlPullParser;
+import org.xmlpull.v1.XmlPullParserFactory;
 import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.StringReader;
 import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
-import java.net.URI;
 import java.net.URL;
-import java.net.URLEncoder;
-import java.util.ArrayList;
-
 
 public class MainActivity extends AppCompatActivity implements OnItemSelectedListener{
 Button b;
+Button b2;
 Spinner stateSpinner;
-String state = "Choose State";
-
+EditText e1;
+EditText e2;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-        android.support.v7.app.ActionBar menu=getSupportActionBar();
+        ActionBar menu=getSupportActionBar();
         menu.setDisplayShowHomeEnabled(true);
         menu.setLogo(R.drawable.ic_action_name);
         menu.setDisplayUseLogoEnabled(true);
+        /////////////////////
+        e1=(EditText)findViewById(R.id.StreetText);
+        String simple = "Enter Street ";
+        String colored = "*";
+        SpannableStringBuilder builder = new SpannableStringBuilder();
+        builder.append(simple);
+        int start = builder.length();
+        builder.append(colored);
+        int end = builder.length();
+        builder.setSpan(new ForegroundColorSpan(Color.RED), start, end, Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
+        e1.setHint(builder);
+        /////////////////////
+        e2=(EditText)findViewById(R.id.CityText);
+        String simple2 = "Enter City ";
+        String colored2 = "*";
+        SpannableStringBuilder builder2 = new SpannableStringBuilder();
+        builder2.append(simple2);
+        int start2 = builder2.length();
+        builder2.append(colored2);
+        int end2 = builder2.length();
+        builder2.setSpan(new ForegroundColorSpan(Color.RED), start2, end2, Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
+        e2.setHint(builder2);
+        //////////////////////
         //spinner
-        stateSpinner = (Spinner) findViewById(R.id.stateSpinner);
-        stateSpinner.setOnItemClickListener((AdapterView.OnItemClickListener) this);
-//http://yuhaoaws-env.elasticbeanstalk.com
-
+        stateSpinner = (Spinner)findViewById(R.id.stateSpinner);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,R.array.states, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        stateSpinner.setAdapter(adapter);
+        b2= (Button)findViewById(R.id.btn2);
+        b2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                e1.setText("");
+                e2.setText("");
+            }
+        });
         b= (Button)findViewById(R.id.btn1);
         b.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                String address = ((EditText)findViewById(R.id.StreetText)).getText().toString();
-                String citytext = ((EditText)findViewById(R.id.CityText)).getText().toString();
-                //  Spinner boxState=(Spinner) findViewById(R.id.stateSpinner);
-                //  String state = boxState.getSelectedItem().toString();
+
+                if(e1.getText().length()==0) {
+                    e1.setError("Street field is required!");
+                }else if(e2.getText().length()==0)
+                {
+                    e2.setError("City field is required!");
+                }else {
+                    String address = e1.getText().toString();
+                    String citytext = e2.getText().toString();
+                    String state = stateSpinner.getSelectedItem().toString();
+                    new HttpRequst().execute(address,citytext,state);
 
 
-                //validate inputs
-                boolean flag = false;
-                if(address.equals("") || citytext.equals("")){
-                    flag = true;}
+                }
 
-                if(flag)
-                    return;//change to popup message
 
-                final  ArrayList<NameValuePair> params=new ArrayList<NameValuePair>();
-                params.add(new BasicNameValuePair("streetAddress", address));
-                params.add(new BasicNameValuePair("city", citytext));
-                params.add(new BasicNameValuePair("state", state));
-                String paramString = URLEncodedUtils.format(params, "utf-8");
-		        //String url = "http://yuhaoaws-env.elasticbeanstalk.com/?"+paramString;
-		        new HttpRequst().execute(paramString);
 
-                Intent i = new Intent(MainActivity.this, SecoundActivity.class);
-               // i.putExtra("key", array1);
-                startActivity(i);
-            }// end on click
+            }
         });
 
 
@@ -86,54 +110,120 @@ String state = "Choose State";
 
     @Override
     public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-        state = stateSpinner.getItemAtPosition(position).toString();
+        stateSpinner.getItemAtPosition(position).toString();
     }
     @Override
     public void onNothingSelected(AdapterView<?> parent) {
     }
 
-}
+    public String[] ParseXML(String xmlString) {
+        String text=null;
+        String []res=new String[6];
+        try {
+                XmlPullParserFactory factory = XmlPullParserFactory.newInstance();
+                factory.setNamespaceAware(true);
+                XmlPullParser parser = factory.newPullParser();
+                parser.setInput(new StringReader(xmlString));
+                int eventType = parser.getEventType();
+                while (eventType != XmlPullParser.END_DOCUMENT) {
+                    String tagname = parser.getName();
+                    switch (eventType) {
+                        case XmlPullParser.START_TAG:
+                          if (tagname.equalsIgnoreCase("address")) {
 
-class HttpRequst extends AsyncTask<String,Void,String>{
+                           }
+                            break;
 
-    @Override
-    protected String doInBackground(String... urls) {
+                        case XmlPullParser.TEXT:
 
-        String url2=urls[0];
-        String response = null;
-    /*    try {
-            URL url = new URL("http://www.zillow.com/webservice/GetSearchResults.htm?zws-id=<X1-ZWz18wlnzn0miz_7oq0o>&address=2114+Bigelow+Ave&citystatezip=Seattle%2C+WA
-");
-            HttpURLConnection con = (HttpURLConnection) url.openConnection();
-           BufferedReader br = new BufferedReader(new inputStreamReader(con.getInputStream()));
+                             text = parser.getText();
+                            break;
 
-            StringBuffer sb = new StringBuffer();
-            String line;
-            while((line=br.readLine())!=null){
-                sb.append(line);
-                break;
+                        case XmlPullParser.END_TAG:
+
+                           if (tagname.equalsIgnoreCase("code")) {
+                               if(Integer.parseInt(text)!=0) {
+                                   return res = null;
+                               }
+
+                            }  else if (tagname.equalsIgnoreCase("street")) {
+                               res[0]=text;
+                           } else if (tagname.equalsIgnoreCase("zipcode")) {
+                               res[1]=text;
+                            } else if (tagname.equalsIgnoreCase("city")) {
+                               res[2]=text;
+                            } else if (tagname.equalsIgnoreCase("state")) {
+                               res[3]=text;
+                            } else if (tagname.equalsIgnoreCase("latitude")) {
+                               res[4]=text;
+                            } else if (tagname.equalsIgnoreCase("longitude")) {
+                               res[5]=text;
+                           }
+                            break;
+                        default:
+                            break;
+                    }
+                    eventType = parser.next();
+                }
+
+            } catch (Exception e) {
+                e.printStackTrace();
             }
-            br.close();
-            return sb.toString();
-        } catch (Exception e) {
-            e.printStackTrace();
+            return res;
 
-        }*/
-        return null;
     }
 
-    @Override
-    protected void onPostExecute(String s) {
+    class HttpRequst extends AsyncTask<String,Void,String>{
+        @Override
+        protected String doInBackground(String... urls) {
 
-        if(s!=null){
+            try {
+                URL url = new URL("http://www.zillow.com/webservice/GetSearchResults.htm?zws-id=X1-ZWz18wlnzn0miz_7oq0o&address="+urls[0]+"+Bigelow+Ave&citystatezip="+urls[1]+"%2C+"+urls[2]);
+                HttpURLConnection con = (HttpURLConnection) url.openConnection();
+                BufferedReader br = new BufferedReader(new InputStreamReader(con.getInputStream()));
+                StringBuffer sb = new StringBuffer();
+                String line;
+                while((line=br.readLine())!=null){
+                    sb.append(line);
+                    break;
+                }
+                br.close();
+                return sb.toString();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            return null;
+        }
 
-          //      JSONArray jsonArray = new JSONArray(s);
-          //    String
-          //  jsonArray = new JSONArray(s);
+
+        @Override
+        protected void onPostExecute(String s) {
 
 
-        }//null condition
+            Bundle b=new Bundle();
+            String[] result =ParseXML(s);
+            if (result!=null) {
+                b.putStringArray("info", result);
+                Intent i = new Intent(MainActivity.this, SecoundActivity.class);
+                i.putExtras(b);
+                startActivity(i);
+            }
+            else {
+                AlertDialog alertDialog = new AlertDialog.Builder(MainActivity.this).create();
+                alertDialog.setTitle("Error");
+                alertDialog.setMessage("please enter the address correctly");
+                alertDialog.setButton(AlertDialog.BUTTON_NEUTRAL, "OK",
+                        new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int which) {
 
-        super.onPostExecute(s);
+                                dialog.dismiss();
+                            }
+                        });
+                alertDialog.show();
+            }
+        }
+
     }
+
 }
+
--- app/src/main/java/com/example/lama/realestateapp/SecoundActivity.java
+++ app/src/main/java/com/example/lama/realestateapp/SecoundActivity.java
@@ -1,5 +1,7 @@
 package com.example.lama.realestateapp;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
@@ -10,12 +12,16 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
 
 import tabs.tab1;
 import tabs.tab2;
 
 public class SecoundActivity extends AppCompatActivity {
-
+    SharedPreferences sharedpreferences;
+    String[] array;
+    public static final String MyPREFERENCES = "MyPrefs" ;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -23,15 +29,23 @@ public class SecoundActivity extends AppCompatActivity {
         // Setup Toolbar
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
+        Bundle b=this.getIntent().getExtras();
+        array=b.getStringArray("info");
         ViewPager viewPager = (ViewPager) findViewById(R.id.pager);
         // Assign created adapter to viewPager
         viewPager.setAdapter(new TabsExamplePagerAdapter(getSupportFragmentManager()));
+        sharedpreferences = getSharedPreferences(MyPREFERENCES, Context.MODE_PRIVATE);
 
         TabLayout tabLayout = (TabLayout) findViewById(R.id.tab_layout);
         // This method setup all required method for TabLayout with Viewpager
-
         tabLayout.setupWithViewPager(viewPager);
+        toolbar.setNavigationIcon(R.drawable.back);
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onBackPressed();
+            }
+        });
     }
 
     public static class TabsExamplePagerAdapter extends FragmentPagerAdapter {
@@ -84,7 +98,15 @@ public class SecoundActivity extends AppCompatActivity {
 
         int id = item.getItemId();
         if (id == R.id.item_one) {
+           SharedPreferences.Editor editor = sharedpreferences.edit();
+            editor.putString("city", array[2]);
+            editor.putString("state", array[3]);
+            editor.putString("street", array[0]);
+            editor.commit();
             item.setIcon(android.R.drawable.btn_star_big_on);
+            //by below line you can get a value from sharedpreferences.
+            // String value = sharedpreferences.getString("city","");
+
             return true;
         }
         return super.onOptionsItemSelected(item);
